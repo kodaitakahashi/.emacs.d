39586f293e9c00571b14966f4c7409d51dd46591
kodaitakahashi <kodaitakahashi.kt@gmail.com>
2015-06-21 22:49:04 +0900

    scala-mode2,ruby-end追加 ruby-modeのインデントの修正

--- init.el
+++ init.el
@@ -99,11 +99,18 @@
 (global-unset-key [left])
 
 ;;global-set-key
+;ウィンドウ移動(下)
 (global-set-key (kbd "C-c <down>") 'windmove-down)
+;ウィンドウ移動(上)
 (global-set-key (kbd "C-c <up>") 'windmove-up)
+;ウィンドウ移動(左)
 (global-set-key (kbd "C-c <left>") 'windmove-left)
+;ウィンドウ移動(右)
 (global-set-key (kbd "C-c <right>") 'windmove-right)
+;eshell起動
 (global-set-key [down] 'eshell)
+;リロード
+(global-set-key (kbd "C-c r") 'eval-buffer)
 
 ;; cask
 (require 'cask "~/.cask/cask.el")
@@ -153,9 +160,28 @@
 ;;ruby-mode
 (autoload 'ruby-mode "ruby-mode"
   "Mode for editing ruby source files" t)
+(add-hook 'ruby-mode '(lambda ()
+	(local-set-key (kbd "RET") 'newline-and-indent)
+))
 (add-to-list 'auto-mode-alist'("\\.rb$latex".ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+;インデントの改良
+(setq ruby-deep-indent-paren-style nil)
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
 
 ;;ruby-electric
 ;(require 'ruby-electric)
@@ -164,15 +190,36 @@
 
 ;;ruby-block----highlight matching block
 (require 'ruby-block)
+;endの対応している行のハイライト
 (ruby-block-mode t)
 (setq ruby-block-highlight-toggle t)
+
+;;ruby-end
+;endや括弧の自動挿入
+(require 'ruby-end)
+(add-hook 'ruby-mode-hook
+  '(lambda()
+   (abbrev-mode 1)
+   (electric-pair-mode t)
+   (electric-indent-mode t)
+   (electric-layout-mode t)))
+
 
 ;;;markdown
 ;; markdown-mode
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
-(setq markdown-command-needs-filename t)		
+(setq markdown-command-needs-filename t) 
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;;scala
+(require 'scala-mode2)
+(add-hook 'scala-mode-hock '(lambda ()
+	(local-set-key (kbd "RET") 'newline-and-indent)
+	(local-set-key (kbd "<backtab>") 'scala-indent:indent-with-reluctant-strategy)
+))
+
 (provide 'init)
+
