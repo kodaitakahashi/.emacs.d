ad4884ffcc98c8358ca193daf500477edf32c7c2
kodaitakahashi <kodaitakahashi.kt@gmail.com>
2015-12-29 17:24:37 +0900

    sass-modeの設定を追加

--- init.el
+++ init.el
@@ -209,9 +209,41 @@
                               (local-set-key (kbd "RET") 'newline-and-indent)
                               (local-set-key (kbd "<backtab>") 'scala-indent:indent-with-reluctant-strategy)
                               ))
+;; CSS
+(defun my-css-electric-pair-brace ()
+  (interactive)
+  (insert "{")(newline-and-indent)
+  (newline-and-indent)
+  (insert "}")
+  (indent-for-tab-command)
+  (previous-line)(indent-for-tab-command)
+  )
+
+(defun my-semicolon-ret ()
+  (interactive)
+  (insert ";")
+  (newline-and-indent))
+
 ;; scss-mode
 (require 'scss-mode)
-(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(scss\\|css\\)\\'" . scss-mode))
+(add-hook 'scss-mode-hook 'ac-css-mode-setup)
+(add-hook 'scss-mode-hook
+          (lambda ()
+            (define-key scss-mode-map "\M-{" 'my-css-electric-pair-brace)
+            (define-key scss-mode-map ";" 'my-semicolon-ret)
+            (setq css-indent-offset 2)
+            (setq scss-compile-at-save nil)
+            (setq ac-sources '(ac-source-yasnippet
+                               ;; ac-source-words-in-same-mode-buffers
+                               ac-source-words-in-all-buffer
+                               ac-source-dictionary
+                               ))
+            (flymake-mode t)
+            ))
+;; sass-mode
+(require 'sass-mode)
+(add-to-list 'auto-mode-alist '("\\.sass$'" . sass-mode))
 ;; slim-mode
 (require 'slim-mode)
 (add-to-list 'auto-mode-alist '("\\.slim$" . slim-mode))
